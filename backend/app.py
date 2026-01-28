0d27759a49e1cb913a6cb627753208d5b9da0edf
UnderfitAgent <60000347+lawun330@users.noreply.github.com>
2026-01-28 19:13:12 +0700

    Fix a typo of missing comma

--- backend/app.py
+++ backend/app.py
@@ -23,7 +23,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:3000",  # React default port
         "https://hexsoftwares-churn-prediction.onrender.com/"  # Render host
-    ]
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
