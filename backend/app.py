6f19ac53d2eb3d4f0618acc7746e606bb52db97e
UnderfitAgent <60000347+lawun330@users.noreply.github.com>
2026-01-28 19:38:12 +0700

    Changed CORS URL

--- backend/app.py
+++ backend/app.py
@@ -22,7 +22,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:3000",  # React default port
-        "https://hexsoftwares-churn-prediction.onrender.com/"  # Render host
+        "https://hexsoftwares-churn-prediction-ui.onrender.com/"  # Render host
     ],
     allow_credentials=True,
     allow_methods=["*"],
